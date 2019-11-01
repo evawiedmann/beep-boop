bd782080236581c10946b39730b19ff77a2d96b4
Eva Wiedmann <evawiedmann@gmail.com>
2019-11-01 13:06:02 -0700

    show answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
 // Business Logic
-var parsedUserInput = parseInt("#userInput")
-
-// expected output: true
+// var parsedUserInput = parseInt("#userInput")
+// console.log(parsedUserInput);
+// var stringInput = parsedUserInput.toString();
+// var splitInput = (stringInput).split('');
 
 // function Computron
-if (splitInput.includes(2)) {
+// if (splitInput.includes("2")) {
 
-console.log("yes");
-} else {
-
-console.log("no");
-}
-var stringInput = parsedUserInput.toString();
-var splitInput = (stringInput).split('');
-
+// console.log("yes");
+// } else {
+//
+// console.log("no");
+// }
 
 // UI
 
@@ -22,9 +20,12 @@ $(document).ready(function () {
     event.preventDefault();
     var userInputVal = $("input#userInput").val();
 
-    $('#stringInput').text(stringInput);
+    console.log(userInputVal);
+
+    // $('#stringInput').text(stringInput);
+    $("#output").text(userInputVal);
     $("#answer").show();
-    // $("#bbForm").hide();
+    $("#bbForm").hide();
 
   });
 
