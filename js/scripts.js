2d9d0e0fc9f48fa56fa0789afa76ba1639e9d7f0
Eva Wiedmann <evawiedmann@gmail.com>
2019-11-01 15:41:07 -0700

    get closer to solving function problems, exliminated some potential pitfalls

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 // Business Logic
-
 var splitInput = userInputVal.split('');
-var userInputVal = $("input#userInput").val();
-
+// var userInputVal = $("input#userInput").val();
 function minusOne(numbers){
   var arrayAnswer = [" "];
   numbers.forEach(function(number) {
@@ -13,30 +11,19 @@ function minusOne(numbers){
 };
 
 // UI
-
 $(document).ready(function () {
   $("form#bbForm").submit(function(event) {
     event.preventDefault();
-
     if (splitInput.includes("3")===true) {
-
       $("#sorry").show();
-
     } else if(splitInput.includes("2")===true) {
-
       $("#boop").show();
-
     } else if(splitInput.includes("1")===true) {
-
       $("#beep").show();
-
     } else {
       // $("#answer").text(arrayAnswer);
       $('#output').text(arrayAnswer);
     }
-
     $("#answer").show();
-
   });
-
 });
